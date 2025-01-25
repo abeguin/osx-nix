337a9b6c5c6a144a2759bb503c11fd70b81d1ffd
abeguin <beguin.arnaud@gmail.com>
2025-01-25 16:33:00 +0100

    first nix-darwin configuration

---
+++ configuration.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+
+{
+
+  imports = [<home-manager/nix-darwin>];
+
+  # Use custom location for configuration.nix.
+  environment.darwinConfig = "$HOME/.config/nix-darwin/configuration.nix";
+
+
+  users.users.abeguin = {
+    name = "abeguin";
+    home = "/Users/abeguin";
+  };
+
+  home-manager.users.abeguin = {
+    home.packages = [
+      pkgs.git
+      pkgs.direnv
+      pkgs.jq
+      pkgs.shellcheck
+      pkgs.taskwarrior3
+    ];
+
+    programs.fish = {
+      enable = true;
+    };
+
+    programs.git = {
+      enable = true;
+
+      userName = "abeguin";
+      userEmail = "beguin.arnaud@gmail.com";
+
+      ignores = [ 
+        "local"
+        ".DS_STORE"
+        ".idea"
+        ".envrc"
+      ];
+
+      lfs = {
+        enable = true;
+      };
+
+      difftastic = {
+        enable = true;
+      };
+    };
+
+    home.stateVersion = "24.11";
+  };
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 5;
+}
