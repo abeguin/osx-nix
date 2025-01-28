cb2ebf81730411ce1fcbc248ad4437651b793801
abeguin <beguin.arnaud@gmail.com>
2025-01-28 08:35:54 +0100

    set nix automatic gc, allow unfree packages

--- configuration.nix
+++ configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 
 {
@@ -8,6 +8,18 @@
   # Use custom location for configuration.nix.
   environment.darwinConfig = "$HOME/.config/nix-darwin/configuration.nix";
 
+
+  # do garbage collection weekly to keep disk usage low
+  nix.gc = {
+    automatic = lib.mkDefault true;
+    options = lib.mkDefault "--delete-older-than 7d";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
 
   users.users.abeguin = {
     name = "abeguin";
