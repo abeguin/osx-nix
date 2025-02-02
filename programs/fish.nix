a205ceaf3e805c76efe7dd6c4890dbd50aaac976
abeguin <afontaine@kleis.ch>
2025-02-02 16:19:07 +0100

    add some plugins to fish

--- programs/fish.nix
+++ programs/fish.nix
@@ -1,1 +1,31 @@
-{ ... }: { programs.fish = { enable = true; }; }
+{ pkgs, ... }: {
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+    '';
+    plugins = [
+      {
+        name = "fish-you-should-use";
+        src = pkgs.fishPlugins.fish-you-should-use.src;
+      }
+      {
+        name = "z";
+        src = pkgs.fishPlugins.z.src;
+      }
+      {
+        name = "git-abbr";
+        src = pkgs.fishPlugins.git-abbr.src;
+      }
+      {
+        name = "fzf";
+        src = pkgs.fishPlugins.fzf-fish.src;
+      }
+      {
+        name = "sponge";
+        src = pkgs.fishPlugins.sponge.src;
+      }
+    ];
+  };
+
+}
