f43de949caa19a9fd8eff8ead947e2e08599a346
abeguin <afontaine@kleis.ch>
2025-02-14 22:53:26 +0100

    enable nix deamon

--- darwin.nix
+++ darwin.nix
@@ -11,9 +11,13 @@
   };
 
   # do garbage collection weekly to keep disk usage low
-  nix.gc = {
-    automatic = lib.mkDefault true;
-    options = lib.mkDefault "--delete-older-than 7d";
+  nix = {
+    enable = true;
+    gc = {
+
+      automatic = lib.mkDefault true;
+      options = lib.mkDefault "--delete-older-than 7d";
+    };
   };
 
   # Allow unfree packages
