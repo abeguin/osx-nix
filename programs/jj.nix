6e7ec51bf50b2ac159c7040fa59266733d726818
Arnaud Fontaine <abeguin@kleis.ch>
2025-02-14 22:51:29 +0100

    update jj config

--- programs/jj.nix
+++ programs/jj.nix
@@ -2,12 +2,19 @@
 
   programs.jujutsu = {
     enable = true;
-
     settings = {
       user = {
         name = "Arnaud Fontaine";
         email = "abeguin@kleis.ch";
       };
+      ui = { pager = "less -FRX"; };
+      revsets = { log = "@ | ancestors(remote_bookmarks().., 2) | trunk()"; };
+    };
+  };
+
+  programs.fish = {
+    shellAbbrs = {
+      jjla = "jj log -r '@ | ancestors(remote_bookmarks()::, 2) | trunk()::'";
     };
   };
 }
