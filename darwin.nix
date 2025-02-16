7cadf61668dd1520204869367c3f3c6c52ab771a
abeguin <afontaine@kleis.ch>
2025-02-16 11:40:33 +0100

    darwin: linux builder exp

--- darwin.nix
+++ darwin.nix
@@ -16,10 +16,22 @@
   # do garbage collection weekly to keep disk usage low
   nix = {
     enable = true;
+    extraOptions = ''
+      extra-platforms = x86_64-darwin aarch64-darwin
+    '';
 
     # Macos Linux builder
-    # linux-builder.enable = true; getting a weird error
-    settings = { trusted-users = [ "@admin" ]; };
+    # linux-builder.enable = true; # getting a weird error
+    settings = {
+      trusted-users = [ "@admin" ];
+      # # - Replace ${ARCH} with either aarch64 or x86_64 to match your host machine
+      # # - Replace ${MAX_JOBS} with the maximum number of builds (pick 4 if you're not sure)
+      # builders =
+      #   "ssh-ng://builder@linux-builder aarch64-linux /etc/nix/builder_ed25519 8 - - - c3NoLWVkMjU1MTkgQUFBQUMzTnphQzFsWkRJMU5URTVBQUFBSUpCV2N4Yi9CbGFxdDFhdU90RStGOFFVV3JVb3RpQzVxQkorVXVFV2RWQ2Igcm9vdEBuaXhvcwo=";
+
+      # # Not strictly necessary, but this will reduce your disk utilization
+      # builders-use-substitutes = true;
+    };
     optimise = { automatic = true; };
 
     # Garbage collection    
