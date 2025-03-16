8d93395afe70fc3e1ab308e40db565b4d5a1bc53
Arnaud Fontaine <afontaine@kleis.ch>
2025-03-16 09:06:33 +0100

    playing with jankyborder

--- darwin/configuration.nix
+++ darwin/configuration.nix
@@ -150,4 +150,12 @@
 
   # Add ability to used TouchID for sudo authentication
   security.pam.services.sudo_local.touchIdAuth = true;
+
+  services = {
+    jankyborders = {
+      enable = true;
+      active_color = "0xAA00ffff";
+      width = 10.0;
+    };
+  };
 }
