3c93d35ed4e6a5bf0087dff6668fccf8d655c7b1
Arnaud Béguin <abeguin@pictet.com>
2025-05-14 16:13:43 +0200

    feat(helix): configure python language

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -2,12 +2,53 @@
   programs.helix = {
     enable = true;
     defaultEditor = true;
-    settings = { theme = "flexoki_dark"; };
-    languages.language = [{
-      name = "nix";
-      auto-format = true;
-      formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
-      language-servers = [ "nil" ];
-    }];
+    settings = {
+      theme = "flexoki_dark";
+      editor = {
+        file-picker = { hidden = false; };
+        clipboard-provider.custom = {
+          yank = {
+            command = "bat";
+            args = [ "tests.txt" ];
+          };
+          paste = {
+            command = "bat";
+            args = [ "tests.txt" ];
+          };
+        };
+      };
+
+    };
+    languages = {
+      # language-server.ruff = {
+      #   command = "uvx";
+      #   args = [ "ruff" "server" ];
+      # };
+      # language-server.basedpyright = {
+      #   command = "uvx";
+      #   args = [ "--from" "basedpyright" "basedpyright-langserver" "--stdio" ];
+      #   config = { python.pythonPath = ".venv/bin/python"; };
+      # };
+      # language = [
+      #   {
+      #     name = "nix";
+      #     auto-format = true;
+      #     formatter.command = "${pkgs.nixfmt-classic}/bin/nixfmt";
+      #     language-servers = [ "nil" ];
+      #   }
+      #   {
+      #     name = "python";
+      #     language-id = "python";
+      #     auto-format = true;
+      #     roots = [ "pyproject.toml" ".git" ".jj" ".venv" ];
+      #     file-types = [ ".py" "ipynb" ];
+      #     # language-servers = [ "ruff" "basedpyright" ];
+      #     formatter = {
+      #       command = "uvx";
+      #       args = [ "ruff" "format" "-" ];
+      #     };
+      #   }
+      # ];
+    };
   };
 }
