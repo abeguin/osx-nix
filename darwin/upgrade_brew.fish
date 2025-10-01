86fed82d15d52cb1989ba518dc3ee559e08e9878
abeguin <afontaine@kleis.ch>
2025-10-01 13:45:38 +0200

    feat: uninstall/install logitech-g-hub instead of upgrading

--- darwin/upgrade_brew.fish
+++ darwin/upgrade_brew.fish
@@ -2,6 +2,25 @@
 
 echo "Fetching list of formulae and casks..."
 
+function uninstall_logitech_g_hub
+    brew uninstall logitech-g-hub
+
+    # 3. Delete system-wide files
+    sudo rm -rf /Applications/lghub.app
+    sudo rm /Library/LaunchAgents/com.logi.ghub.plist
+    sudo rm /Library/LaunchDaemons/com.logi.ghub.updater.plist
+    sudo rm -rf /Users/Shared/LGHUB
+    sudo rm -rf /Users/Shared/.logishrd
+
+    # 4. Delete files from user directory
+    sudo rm -rf ~/Library/Application\ Support/lghub
+    sudo rm -rf /Users/afo/Library/Preferences/com.logi.ghub.ui.plist
+
+    sudo rm -rf /opt/homebrew/Caskroom/logitech-g-hub
+end
+
+uninstall_logitech_g_hub
+
 # Upgrade formulae
 for formula in (brew list)
     echo "Upgrading formula: $formula"
@@ -15,6 +34,9 @@ for cask in (brew list --cask)
 end
 
 echo "Cleaning up..."
+brew cleanup
+
+brew install --cask logitech-g-hub
 brew cleanup
 
 echo "Upgrade complete!"
