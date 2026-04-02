f89b5afc10f9ce87415a61b7f18f5bb5035dee8f
abeguin <afontaine@kleis.ch>
2026-04-02 17:16:50 +0200

    add fish function to display jj stuff

--- modules/home-manager/fish.nix
+++ modules/home-manager/fish.nix
@@ -32,6 +32,45 @@ in
       find = "fd";
       br = "broot";
     };
+    functions = {
+      fish_vcs_prompt = ''
+        # If a prompt succeeded, we assume that it's printed the correct info.
+        # This is so we don't try svn if git already worked.
+        fish_jj_prompt $argv
+        or fish_git_prompt $argv
+        # or fish_hg_prompt $argv
+        # The svn and fossil prompts are disabled by default because they can be quite slow.
+        # To enable them uncomment them.
+        # You can also only use them in specific directories by checking $PWD.
+        # or fish_fossil_prompt $argv
+        # or fish_svn_prompt
+      '';
+      fish_jj_prompt = ''
+        if not command -sq jj
+            return 1
+        end
+        set -l info "$(
+            jj log 2>/dev/null --no-graph --ignore-working-copy --color=always --revisions @ \
+                --template '
+                    separate(" ",
+                        coalesce(
+                            if(!bookmarks, ""),
+                            bookmarks.map(|b| b.name()).join(" ")
+                        ),
+                        change_id.shortest(6),
+                        if(description, description.first_line(), label("empty_desc", "(no desc)")),
+                        if(empty, label("empty", "(empty)")),
+                        if(conflict, label("conflict", "×")),
+                        if(divergent, label("divergent", "!")),
+                    )
+                '
+        )"
+        or return 1
+        if test -n "$info"
+            printf ' (%s)' $info
+        end
+      '';
+    };
   };
 
 }
